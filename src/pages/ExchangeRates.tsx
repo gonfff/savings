d22b2c0ab8a368b472f1ba0ba77e9966396bd707
Vladislav Isupov <soup.ae@gmail.com>
2024-12-14 16:55:38 +0300

    Add forms (#2)
    * add form hook and example of usage
    
    * remove comments
    
    * rename file
    
    * fix init value of date in form constructor
    
    ---------
    
    Co-authored-by: Vladislav Isupov <isupovvi95@gmail.com>

--- src/pages/ExchangeRates.tsx
+++ src/pages/ExchangeRates.tsx
@@ -1,7 +1,7 @@
 import { createEffect, createSignal, For, onCleanup, onMount } from 'solid-js';
-import { TableRowButton } from '../components/Buttons.tsx';
-import { FormInput } from '../components/Form.tsx';
-import { ModalWindow } from '../components/ModalWindow.tsx';
+import { TableRowButton } from '../components/buttons.tsx';
+import { FormInput } from '../components/FormInput.tsx';
+import useForm from '../hooks/UseForm.ts';
 import {
   ExchangeRate,
   fetchExchangeRates,
@@ -30,43 +30,176 @@ const ExchangeRatesHeader = () => {
   );
 };
 
+type ModalTextProps = {
+  text: string;
+};
+
+const ModalTitle = (props: ModalTextProps) => {
+  return <h3 class="font-bold text-lg">{props.text}</h3>;
+};
+
+const ModalText = (props: ModalTextProps) => {
+  return <p class="text-sm opacity-50">{props.text}</p>;
+};
+
 const AddRateForm = () => {
-  const handleSave = (formData: Record<string, string>) => {
-    console.log('Form Data:', formData);
+  const [isOpen, setIsOpen] = createSignal(false);
+
+  // let formConstructor = [
+  //   {
+  //     name: 'source',
+  //     placeholder: 'AAPL',
+  //     type: 'text',
+  //     setFormData,
+  //     value: 'QWERT',
+  //   },
+  //   {
+  //     name: 'target',
+  //     placeholder: 'USD',
+  //     type: 'text',
+  //     setFormData,
+  //     value: 'RUB',
+  //   },
+  //   {
+  //     name: 'rate',
+  //     placeholder: '3.123',
+  //     type: 'float',
+  //     setFormData,
+  //     value: '1',
+  //   },
+  //   {
+  //     name: 'date',
+  //     type: 'text',
+  //     setFormData,
+  //     value: '20-12-2024',
+  //   },
+  // ];
+  const formConstructor = [
+    {
+      name: 'source',
+      placeholder: 'AAPL',
+      type: 'text',
+      value: 'QWERT',
+    },
+    {
+      name: 'target',
+      placeholder: 'USD',
+      type: 'text',
+      value: 'RUB',
+    },
+    {
+      name: 'rate',
+      placeholder: '3.123',
+      type: 'float',
+      value: '1',
+    },
+    {
+      name: 'date',
+      type: 'date',
+      value: '2024-12-20',
+    },
+  ];
+
+  const formData = formConstructor.reduce(
+    (acc, cur) => ({ ...acc, [cur.name]: cur.value }),
+    {},
+  );
+
+  const { form, updateFormField, submit, setDefaultValues, clearField } =
+    useForm(formData);
+
+  const handleSubmit = (event: Event): void => {
+    event.preventDefault();
+    setIsOpen(false);
+    submit(form);
+    setDefaultValues();
   };
 
+  createEffect(() => {
+    if (form.sameAsAddress) {
+      clearField('shippingAddress');
+    }
+  });
+
   return (
-    <ModalWindow
-      title="Add Exchange Rate"
-      text="Dont forget to add assets first"
-      triggerButton={<button class="btn btn-primary">Add Rate</button>}
-      actionButtonCallback={handleSave}
-    >
-      {(setFormData) => (
-        <>
-          <FormInput
-            name="source"
-            placeholder="AAPL"
-            type="text"
-            setFormData={setFormData}
-          />
-          <FormInput
-            name="target"
-            placeholder="USD"
-            type="text"
-            setFormData={setFormData}
-          />
-          <FormInput
-            name="rate"
-            placeholder="3.123"
-            type="number"
-            setFormData={setFormData}
-          />
-          <FormInput name="date" type="date" setFormData={setFormData} />
-        </>
+    <>
+      <button class="btn btn-primary" onClick={() => setIsOpen(true)}>
+        Add Rate
+      </button>
+
+      {isOpen() && (
+        <div
+          class="modal modal-open"
+          onClick={(e) => {
+            if (e.target === e.currentTarget) setIsOpen(false);
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            <div class="modal-box">
+              <ModalTitle text="Add Exchange Rate" />
+              <ModalText text="Dont forget to add assets first" />
+              {
+                <>
+                  {formConstructor.map((item) => {
+                    return (
+                      <FormInput
+                        name={item.name}
+                        type={item.type}
+                        placeholder={item.placeholder}
+                        value={form[item.name]}
+                        updateFormField={updateFormField}
+                      />
+                    );
+                  })}
+                </>
+              }
+              <div class="modal-action">
+                <button class="form-submit btn btn-primary" type="submit">
+                  Save
+                </button>
+                <button class="btn" onClick={() => {}}>
+                  Close
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       )}
-    </ModalWindow>
+    </>
   );
+
+  // return (
+  //   <ModalWindow
+  //     title="Add Exchange Rate"
+  //     text="Dont forget to add assets first"
+  //     triggerButton={<button class="btn btn-primary">Add Rate</button>}
+  //     actionButtonCallback={handleSave}
+  //   >
+  //     {(setFormData) => (
+  //       <>
+  //         <FormInput
+  //           name="source"
+  //           placeholder="AAPL"
+  //           type="text"
+  //           setFormData={setFormData}
+  //         />
+  //         <FormInput
+  //           name="target"
+  //           placeholder="USD"
+  //           type="text"
+  //           setFormData={setFormData}
+  //         />
+  //         <FormInput
+  //           name="rate"
+  //           placeholder="3.123"
+  //           type="number"
+  //           setFormData={setFormData}
+  //         />
+  //         <FormInput name="date" type="date" setFormData={setFormData} />
+  //       </>
+  //     )}
+  //   </ModalWindow>
+  // );
 };
 
 const ExchangeRatesTable = () => {
